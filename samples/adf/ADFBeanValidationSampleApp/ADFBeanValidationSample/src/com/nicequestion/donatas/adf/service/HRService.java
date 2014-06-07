d206ad1bffb6bc9e15e88a6c8a2757dee81723d1
Donatas Valys <dvalys@gmail.com>
2014-06-07 23:25:18 +0200

    Update HRService.java
    not used imports removed

--- samples/adf/ADFBeanValidationSampleApp/ADFBeanValidationSample/src/com/nicequestion/donatas/adf/service/HRService.java
+++ samples/adf/ADFBeanValidationSampleApp/ADFBeanValidationSample/src/com/nicequestion/donatas/adf/service/HRService.java
@@ -6,10 +6,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
-/*import oracle.adfinternal.controller.state.AdfcContext;
-import oracle.adfinternal.controller.state.PageFlowStack;
-import oracle.adfinternal.controller.state.PageFlowStackEntry;*/
-
 public class HRService {
     
     private List<Employee> employeeList;
